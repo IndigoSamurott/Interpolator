ba886e9aec3cb1f427fac4833d51294d360d990c
Sagar <iamsagarnair@gmail.com>
2023-07-31 12:38:45 +0100

    get_optical_flow() now converts to greyscale & writes to file

--- main.py
+++ main.py
@@ -14,14 +14,32 @@ import cv2
 
 
 def get_optical_flow(cap):
+
+    #get data for VideoWriter
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    fWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    fHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    #create VideoWriter object
+    out = cv2.VideoWriter('grey.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (fWidth, fHeight), isColor=False)
+
+
     # get frames
     while True:
         successfully_read, frame = cap.read()
         if not successfully_read:
             break
+
+        #make grey
+        greyVer = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
+        #tests
+        print(greyVer)
+        print(greyVer[100][200])
+        out.write(greyVer)
        
-        # make frames gray
+    out.release()
+    cap.release()
         # graph the frames
         
         # get Lucas Kanade feature paramters for frames
@@ -33,7 +51,6 @@ def main():
     file_name = "example.mp4"
     cap = cv2.VideoCapture(file_name)
     optical_flow = get_optical_flow(cap)
-
     
 
 
